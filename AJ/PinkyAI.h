832d006ec35318c38e9ce0f58be618bcf0d000b0
AJBussman26 <48929587+AJBussman26@users.noreply.github.com>
2019-04-01 14:02:39 -0700

    Finished BlinkyAI and PinkyAI

---
+++ AJ/PinkyAI.h
@@ -0,0 +1,49 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	PinkyAI.h
+// Author(s):	A.J. Bussman (anthony.bussman)
+// Project:		PAC-MAN
+// Course:		WANIC VGP2 2018-2019
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+#pragma once
+
+//------------------------------------------------------------------------------
+// Include Files:
+//------------------------------------------------------------------------------
+
+#include "BaseAI.h"
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Structures:
+//------------------------------------------------------------------------------
+
+namespace Behaviors
+{
+
+	class PinkyAI : public BaseAI
+	{
+	public:
+		//------------------------------------------------------------------------------
+		// Public Functions:
+		//------------------------------------------------------------------------------
+
+		// Initialize this component (happens at object creation).
+		void Initialize() override;
+
+	protected:
+		//------------------------------------------------------------------------------
+		// Protected Functions:
+		//------------------------------------------------------------------------------
+
+		// Called when the AI should choose a target.
+		// Params:
+		//   adjacentTiles = An array of adjacent empty tiles.
+		//   emptyCount = How many empty tiles were found.
+		virtual void OnTarget(AdjacentTile adjacentTiles[4], size_t emptyCount);
+	};
+}
